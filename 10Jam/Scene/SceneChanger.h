a466b67b5b6c3e2c2ab0bedd0d19eec8f9d10acb
koyamakaito <k020g1197@g.neec.ac.jp>
2023-09-05 15:15:28 +0900

    Merge pull request # 4 from sekishotaro/addSoundClass

---
+++ 10Jam/Scene/SceneChanger.h
@@ -0,0 +1,32 @@
+#pragma once
+class SceneChanger {
+public:
+	/// <summary>
+	/// インストラクタ
+	/// </summary>
+	SceneChanger();
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~SceneChanger();
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update();
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+private:
+	float frame_ = 0.f, kFrameMax = 45.f, frameNow_ = 0.0f;
+	float r = 0.f;
+	bool isStart = true;
+	bool isClose = false;
+
+};
+
