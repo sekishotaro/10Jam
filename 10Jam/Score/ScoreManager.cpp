f6868cd0b0be3828f0e289bf2c4a3f6d14f6c027
koyamakaito <k020g1197@g.neec.ac.jp>
2023-09-07 16:18:05 +0900

    【スコアそれっぽく】最悪ゲーム利用できるフォント感を出しました

--- 10Jam/Score/ScoreManager.cpp
+++ 10Jam/Score/ScoreManager.cpp
@@ -10,5 +10,7 @@ ScoreManager* ScoreManager::GetInstance() {
 
 void ScoreManager::Draw() {
 	SetFontSize(64);
+	DrawFormatString(895, 25, GetColor(100, 100, 100), "SCORE:%d", score);
 	DrawFormatString(900, 20, GetColor(255,255,255),"SCORE:%d", score);
+
 }
