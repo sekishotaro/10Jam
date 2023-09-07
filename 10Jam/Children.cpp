59e1781c12bbdbcc92f78037a849dc3d93216533
sekishotaro <k020g1492@g.neec.ac.jp>
2023-09-07 10:25:30 +0900

    スクロール修正

--- 10Jam/Children.cpp
+++ 10Jam/Children.cpp
@@ -96,9 +96,7 @@ void Children::TracColProcess() {
 }
 
 void Children::TrackMove() {
-	XMFLOAT2 scroll = ScrollManager::GetInstance()->GetMove();
-	XMFLOAT2 moveVec = { player_->GetMoveVec().x + scroll.x,player_->GetMoveVec().y + scroll.y };
-	restrainMoveVec.push_back(moveVec);
+	restrainMoveVec.push_back(player_->GetMoveVec());
 
 	if (restrainMoveVec.size() < (13 * restraintTh)) return;
 
